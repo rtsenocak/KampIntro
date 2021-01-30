06f870fb63af11cca84ee1f4f4976ed2a2c4e97b
Recep Tayyip Şenocak <rtsenocak@gmail.com>
2021-01-30 14:47:28 +0300

    20 ocağın tekrarını 30 ocak'ta izledim

---
+++ Kampİntro/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Kampİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faziOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun < dolarBugun) 
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarlar butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
---
+++ Odev/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Urun urun1 = new Urun();
+            urun1.urunAdi = ("Mi 10T Pro");
+            urun1.urunTuru = ("Telefon");
+            urun1.urunFiyatı = (5000);
+
+            Urun urun2 = new Urun();
+            urun2.urunAdi = ("Mi 10T Lite");
+            urun2.urunTuru = ("Telefon");
+            urun2.urunFiyatı = (3800);
+
+            Urun urun3 = new Urun();
+            urun3.urunAdi = ("Mi 10T");
+            urun3.urunTuru = ("Telefon");
+            urun3.urunFiyatı = (4500);
+
+
+
+            //Console.WriteLine(urun1.urunAdi+" "+ urun1.urunTuru+" : "+urun1.urunFiyatı );
+
+            Urun[] urunler = new Urun[] {urun1, urun2, urun3};
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.urunAdi+" "+urun.urunTuru+" : "+urun.urunFiyatı);
+            }
+
+        }
+
+        class Urun
+        {
+            public string urunAdi { get; set; }
+            public string urunTuru { get; set; }
+            public int urunFiyatı { get; set; }
+        }
+    }
+}
